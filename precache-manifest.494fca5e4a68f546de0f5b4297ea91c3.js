ca4086f8598ee598113b9533f4c37381c2fc1074
Benjamin Kayode <benjamin.node@gmail.com>
2019-06-19 17:43:40 +0100

    Updates

---
+++ precache-manifest.494fca5e4a68f546de0f5b4297ea91c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a12c80f24d99f72d56ff172630a8b25",
+    "url": "/react-github/index.html"
+  },
+  {
+    "revision": "28ad7be3925b23d6c506",
+    "url": "/react-github/static/css/main.d77029d2.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/react-github/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "28ad7be3925b23d6c506",
+    "url": "/react-github/static/js/main.351a1b66.chunk.js"
+  },
+  {
+    "revision": "289121d634d31a32edd1",
+    "url": "/react-github/static/js/runtime~main.452640b1.js"
+  }
+]);
